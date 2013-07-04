26ca4e5f999be3d895299887678046afdba58660
Alex Shepard <alex@meatfreezer.com>
2013-07-04 10:29:09 -0700

    turning my back on rrrrrails

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Spimr::Application.config.secret_token = '2aed497ee911c22e3eb5ddde70a541af5fbaae7998f6f9600c7fde602a08ec6260f14bbdb98affac7f21c097d9dedb5eda50922bbf7044e1b7092b447bb38f6d'
--- db/migrate/20130601200952_add_uuid_to_spime.rb
+++
@@ -1,5 +0,0 @@
-class AddUuidToSpime < ActiveRecord::Migration
-  def change
-    add_column :spimes, :uuid, :string
-  end
-end
