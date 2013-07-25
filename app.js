0045c2bfc4b57b81b47637f225c15fa97937e4b8
Alex Shepard <alex@meatfreezer.com>
2013-07-24 17:57:43 -0700

    remove nodefly - meh

--- app.js
+++ app.js
@@ -3,13 +3,6 @@
  * Module dependencies.
  */
 
-if (process.env.NODEFLY_APPLICATION_KEY) {
-  require('nodefly').profile(
-    process.env.NODEFLY_APPLICATION_KEY,
-    [process.env.APPLICATION_NAME,'Heroku']
-  );
-}
-
 if(process.env.NODETIME_ACCOUNT_KEY) {
   require('nodetime').profile({
     accountKey: process.env.NODETIME_ACCOUNT_KEY,
