bd492cf893cb225b3d782c31c0e5416ed37fbdc9
Alex Shepard <alex@meatfreezer.com>
2013-08-19 16:20:43 -0700

    cleanup suggested by jslint

--- app.js
+++ app.js
@@ -1,32 +1,38 @@
+/*jslint node: true */
+/*jslint es5: true */
+/*jslint indent: 2 */
+/*jslint nomen: true */
 
 /**
  * Module dependencies.
  */
 
-if(process.env.NODETIME_ACCOUNT_KEY) {
+"use strict";
+
+if (process.env.NODETIME_ACCOUNT_KEY) {
   require('nodetime').profile({
     accountKey: process.env.NODETIME_ACCOUNT_KEY,
     appName: 'Spimr'
   });
 }
 
-require('coffee-script')
+require('coffee-script');
 
-var express = require('express')
-  , routes = require('./routes')
-  , flash = require('connect-flash')
-  , http = require('http')
-  , RedisStore = require('connect-redis')(express)
-  , mongoose = require('mongoose')
-  , twitter = require('twitter')
-  , path = require('path');
+var express = require('express');
+var routes = require('./routes');
+var flash = require('connect-flash');
+var http = require('http');
+var RedisStore = require('connect-redis')(express);
+var mongoose = require('mongoose');
+var Twitter = require('twitter');
+var path = require('path');
 
 require('express-namespace');
-User = require('./models/user.coffee');
+require('./models/user.coffee');
 
 var app = express();
 
-app.twit = new twitter({
+app.twit = new Twitter({
   consumer_key:         process.env.TWITTER_CONSUMER_KEY,
   consumer_secret:      process.env.TWITTER_CONSUMER_SECRET,
   access_token_key:     process.env.TWITTER_ACCESS_TOKEN_KEY,
@@ -38,8 +44,8 @@ var redis;
 
 if (process.env.REDISTOGO_URL) {
   // Heroku redistogo connection
-  rtg   = require('url').parse(process.env.REDISTOGO_URL)
-  redis = require('redis').createClient(rtg.port, rtg.hostname)
+  var rtg = require('url').parse(process.env.REDISTOGO_URL);
+  redis = require('redis').createClient(rtg.port, rtg.hostname);
   redis.auth(rtg.auth.split(':')[1]); // auth 1st part is username and 2nd is password separated by ":"
 } else {
   // localhost
@@ -50,7 +56,7 @@ if (process.env.REDISTOGO_URL) {
 app.set('port', process.env.PORT || 3000);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
-app.set('storage-uri', process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://localhost/spimr')
+app.set('storage-uri', process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://localhost/spimr');
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
@@ -63,13 +69,13 @@ app.use(express.session({
   store: new RedisStore({client: redis})
 }));
 
-app.use(function(req, res, next){
-  res.locals.user_id = req.session.user_id
-  res.locals.user_email = req.session.user_email
+app.use(function (req, res, next) {
+  res.locals.user_id = req.session.user_id;
+  res.locals.user_email = req.session.user_email;
   res.locals.path = req.path;
   res.locals.google_analytics_account = process.env.GOOGLE_ANALYTICS_ACCOUNT;
   res.locals.google_maps_key = process.env.GOOGLE_MAPS_KEY;
-  res.locals.moment = require('moment')
+  res.locals.moment = require('moment');
   next();
 });
 
@@ -77,13 +83,13 @@ app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 
 // development only
-if ('development' == app.get('env')) {
+if ('development' === app.get('env')) {
   app.use(express.errorHandler());
 }
 
 // mongoose
 var err;
-mongoose.connect(app.get('storage-uri'), { db: {save: true }}, (err), function() {
+mongoose.connect(app.get('storage-uri'), { db: {save: true }}, (err), function () {
   if (err) {
     console.log("Mongoose - connection error: " + err);
     return;
@@ -101,6 +107,6 @@ require('./apps/account/routes.coffee')(app);         // account details
 require('./apps/authentication/routes.coffee')(app);  // session stuff, signin, signout
 require('./apps/checkin/routes.coffee')(app);         // spime sighting handler
 
-http.createServer(app).listen(app.get('port'), function(){
+http.createServer(app).listen(app.get('port'), function () {
   console.log('Express server listening on port ' + app.get('port'));
 });
